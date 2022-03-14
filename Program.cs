d84a8ccf0daa74949ee416624461be22a1c6d7d0
theAgingApprentice <TheAgingApprentice@protonmail.com>
2022-03-13 21:56:22 -0400

    Set up starting code base

---
+++ Program.cs
@@ -0,0 +1,162 @@
+﻿/// Programmer notes:
+/// Create: https://docs.microsoft.com/en-us/dotnet/core/tutorials/with-visual-studio-code?pivots=dotnet-6-0
+/// Run: donet run in directory where *.csproj file resides or ./name where binary resides.
+/// Publish: https://docs.microsoft.com/en-us/dotnet/core/tutorials/publishing-with-visual-studio-code?pivots=dotnet-6-0
+/// Passing a variable number of parameters to a method: https://www.c-sharpcorner.com/UploadFile/manas1/params-in-C-Sharp-pass-variable-number-of-parameters-to-method/
+/// Before running be sure to save the file. This is NOT done automatically in VSC!
+/// To run be in src directry and type "dotnet run".
+namespace HexbotCompiler
+{
+   /// <summary>
+   /// This class manages the Hexbot template file. Propertylist:
+   /// <list type="Properties">
+   /// <item>
+   /// <description>getSrcContent</description>
+   /// </item>
+   /// <item>
+   /// <description>templateName</description>
+   /// </item>
+   /// </list>
+   /// </summary>
+   class templateFile
+   {
+      public string srcFileName = "template.txt";
+      private string[] srcLines = {""}; 
+      /// <summary>
+      /// Puts each line of the template file into an element of a string array.
+      /// <summary>
+      /// <param name="null">Does not return any values.</param>
+      /// <returns>A string array</returns>
+      /// <exception cref="DataException">Thrown if the file name provided does not exist.</exception>
+      public string[] getSrcContent()
+      {
+         if (File.Exists(srcFileName)) 
+         {
+            try 
+            {
+               srcLines = System.IO.File.ReadAllLines(srcFileName);
+            } // try 
+            catch(FileNotFoundException e) 
+            {
+               Console.WriteLine($"Attempted read of {srcFileName} resulted in error {e}.");
+            } // catch() 
+         } // if
+         else
+         {
+            Console.WriteLine($"Template file {srcFileName} does not exists.");
+         } // else
+         return srcLines;   
+      } // getSrcContent()
+
+      /// <summary>
+      /// Displays the content of the source template file to the console.
+      /// <summary>
+      /// <param name="null">Does not accept any arguments.</param>
+      /// <returns>Does not return any values</returns>
+      /// <exception cref="NoException">Does not throw any exceptions.</exception>
+      public void displaySrcContent()
+      {
+         System.Console.WriteLine($"Content of template source file called {srcFileName}... ");
+         foreach(string line in srcLines)
+         {
+            Console.WriteLine("-->" + line);
+         } // foreach()
+      } // displayContent()
+
+   } // class templateFile
+
+   /// <summary>
+   /// This class manages the Hexbot script file. Propertylist:
+   /// <list type="Properties">
+   /// <item>
+   /// <description>getSrcContent</description>
+   /// </item>
+   /// </list>
+   /// </summary>
+   class scriptFile
+   {
+      private string[] srcLines = {""};   
+      private string srcFileName = "";
+
+      /// <summary>
+      /// Puts each line of the script file into an element of a string array.
+      /// <summary>
+      /// <param name="null">Does not return any values.</param>
+      /// <returns>A string array</returns>
+      /// <exception cref="DataException">Thrown if the file name provided does not exist.</exception>
+      public string[] getSrcContent()
+      {
+         bool scriptFileNotFound  = true;
+         while(scriptFileNotFound)
+         {
+            Console.WriteLine("Please enter the script file name:");
+            var userInput = Console.ReadLine();
+            srcFileName = userInput?.ToString() ?? ""; // Convert input to string (no null). 
+            try 
+            {
+               srcLines = System.IO.File.ReadAllLines(srcFileName);
+               scriptFileNotFound = false;    
+            } // try 
+            catch(FileNotFoundException e) 
+            {
+               Console.WriteLine($"Attempted read of {srcFileName} resulted in error {e}.");
+            } // catch() 
+         } // while
+         return srcLines;   
+      } // getSrcContent()
+
+      /// <summary>
+      /// Displays the content of the source template file to the console.
+      /// <summary>
+      /// <param name="null">Does not accept any arguments.</param>
+      /// <returns>Does not return any values</returns>
+      /// <exception cref="NoException">Does not throw any exceptions.</exception>
+      public void displaySrcContent()
+      {
+         System.Console.WriteLine($"Content of script source file called {srcFileName}... ");
+         foreach(string line in srcLines)
+         {
+            Console.WriteLine("-->" + line);
+         } // foreach()
+      } // displayContent()      
+   } // class scriptFile
+
+   /// <summary>
+   /// Generates MQTTfx files that control the Hexbot legs.
+   /// <list type="Properties">
+   /// <item>
+   /// <description>Main</description>
+   /// </item>
+   /// </list>
+   /// </summary>   
+   class Program
+   {
+      /// <summary>
+      /// This is the main logic driving this program.
+      /// <summary>
+      /// <param name="args">string array containing command line arguments.</param>
+      /// <returns>null</returns>
+      static void Main(string[] args)
+      {
+         var template = new templateFile();
+         var script = new scriptFile(); 
+         Console.Clear();
+         template.srcFileName = "template.txt";
+         string[] scriptLines = script.getSrcContent();
+         script.displaySrcContent();
+         string[] templateLines = template.getSrcContent();
+         template.displaySrcContent();
+      } // Main()
+   } // class Program
+} // namespace HexbotCompiler
+
+/*
+//         string scriptName = uInput?.ToString() ?? "";
+
+
+//         var name = Console.ReadLine();
+//         var currentDate = DateTime.Now;
+//         Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+//         Console.Write($"{Environment.NewLine}Press any key to exit...");
+//         Console.ReadKey(true);
+*/
--- src/readme.md
+++
@@ -1,3 +0,0 @@
-# About the src directory
-
-This directory contains the source files used by the c++ compiler to build binares. There should only be this readme.md file, the main.dox file, *.h files, *.cpp files and *.txt files located in this directory. The *.txt files may eventually move.
